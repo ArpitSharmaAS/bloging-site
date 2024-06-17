7e815063e0e65d30c01bf8c7b121b9363ea7fd31
arpitsharmaaaa <arpitsharmably18@gmail.com>
2024-06-17 08:36:21 +0530

    validation add

--- controllers/AuthorController.js
+++ controllers/AuthorController.js
@@ -1,6 +1,33 @@
 const authorModel = require('../Models/AuthorModel');
-
+const {isempty} = require('../validator/validation')
 const createAuthor = async (req, res)=>{
     const data = req.body;
     const {fName,lName,title,email,password} = data
+    if(Object.keys(data).length == 0){
+        return res.status(400).send({status: false, message: "all fields are mandatory"})
+    }
+
+    if(!fName){
+        return res.status(400).send({status: false, message: "first name not found"})
+    }
+
+    if(!isempty(lName)){
+        return res.status(400).send({status: false, message: "last name not found"})
+    }
+
+    if(!isempty(title)){
+        return res.status(400).send({status: false, message: "title not found"})
+    }else{
+        if(title != "Mr" || title != "Mrs" || title != "Miss"){
+                 return res.status(400).send({status: false, message: "title only be Mr, Mrs, Miss"})
+        }
+    }
+
+    if(!isempty(email)){
+        return res.status(400).send({status: false, message: "email not found"})
+    }
+
+    if(!isempty(password)){
+        return res.status(400).send({status: false, message: "plz enter password"})
+    }
 }
---
+++ validator/validation.js
@@ -0,0 +1,27 @@
+const { model } = require("mongoose")
+
+const isempty = (value)=>{
+    if(typeof value === "undefined" || value === null ){
+        return false
+    }
+
+    if(typeof value === "string" && value.trim().length === 0){
+        return false
+    }
+}
+
+const isValidName = (value)=>{
+    const nameRegex = /^[a-zA-z]+$/
+    return nameRegex.test(value)
+}
+
+const isValidEmail = (value)=>{
+    const emailRegex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return emailRegex.test(value)
+}
+
+const isValidPassword = (value)=>{
+    const passwordRegex = /^(?=.\d)(?=.[!@#$%^&])(?=.[a-z])(?=.*[A-Z]).{8,}$/;
+    return passwordRegex.test(value)
+}
+module.exports = {isempty}
